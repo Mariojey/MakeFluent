2d67d1f351aea475fccb8fe79885205c5e97ca80
Mariusz Jacek <marios.yaceg@gmail.com>
2022-11-26 12:56:13 +0100

    bugfix

--- src/pages/germany/data3.js
+++ src/pages/germany/data3.js
@@ -83,5 +83,93 @@ export default [{
         id: 18,
         word: 'Das ist ja schade',
         translating: 'Szkoda'
+    }, {
+        id: 19,
+        word: 'Antibiotikum',
+        translating: 'antybiotyk'
+    }, {
+        id: 20,
+        word: 'Arzt',
+        translating: 'lekarz'
+    }, {
+        id: 21,
+        word: 'Attest',
+        translating: 'zwolnienie lekarskie'
+    }, {
+        id: 22,
+        word: 'Diagnose',
+        translating: 'diagnoza'
+    }, {
+        id: 23,
+        word: 'Entschuldigung',
+        translating: 'usprawiedliwienie'
+    }, {
+        id: 24,
+        word: 'Gesundheitskarte',
+        translating: 'kartazdrowia'
+    }, {
+        id: 25,
+        word: 'Halsentzündung',
+        translating: 'Zapalenie gardła'
+    }, {
+        id: 26,
+        word: 'Halsschmerzen',
+        translating: 'Bóle gardła'
+    }, {
+        id: 27,
+        word: 'Hausarzt',
+        translating: 'lekarz rodzinny'
+    }, {
+        id: 28,
+        word: 'Praxis',
+        translating: 'gabinet lekarski'
+    }, {
+        id: 29,
+        word: 'Rezept',
+        translating: 'recepta'
+    }, {
+        id: 30,
+        word: 'Sirup',
+        translating: 'syrop'
+    }, {
+        id: 31,
+        word: 'Sprechstunde',
+        translating: 'Godziny przyjęć'
+    }, {
+        id: 32,
+        word: 'Tablette',
+        translating: 'tabletka'
+    }, {
+        id: 33,
+        word: 'Termin',
+        translating: 'termin'
+    }, {
+        id: 34,
+        word: 'sich frei machen',
+        translating: 'rozbierać się'
+    }, {
+        id: 35,
+        word: 'untersuchen',
+        translating: 'badać'
+    }, {
+        id: 36,
+        word: 'verschreiben',
+        translating: 'przepisywać'
+    }, {
+        id: 37,
+        word: 'ein Attest für die Schule',
+        translating: 'zwolnienie lekarskie do szkoły'
+    }, {
+        id: 38,
+        word: 'zur Kontrolle kommen',
+        translating: 'przyjść do kontroli'
+    }, {
+        id: 39,
+        word: 'in die Sprechstunde kommen',
+        translating: 'przyjść na wizytę'
+    }, {
+        id: 40,
+        word: 'einen Termin aus machen',
+        translating: 'umówić termin'
     }
 ]
