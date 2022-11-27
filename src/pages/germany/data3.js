7f0b21057acdb42c721126bfab9453f9427b4cbb
Mariusz Jacek <marios.yaceg@gmail.com>
2022-11-27 12:47:37 +0100

    Add English Dictionary

--- src/pages/germany/data3.js
+++ src/pages/germany/data3.js
@@ -171,5 +171,177 @@ export default [{
         id: 40,
         word: 'einen Termin aus machen',
         translating: 'umówić termin'
+    }, {
+        id: 41,
+        word: 'Beutel',
+        translating: 'saszetka z lekiem'
+    }, {
+        id: 42,
+        word: 'Medikament',
+        translating: 'lekarstwo'
+    }, {
+        id: 43,
+        word: 'Nasentropfen',
+        translating: 'Pulver'
+    }, {
+        id: 44,
+        word: 'Schmerztablette',
+        translating: 'tabletka przeciwbólowa'
+    }, {
+        id: 45,
+        word: 'Tropfen',
+        translating: 'kropla'
+    }, {
+        id: 46,
+        word: 'auflösen',
+        translating: 'rozpuszczać'
+    }, {
+        id: 47,
+        word: 'dosieren',
+        translating: 'dawkować'
+    }, {
+        id: 48,
+        word: 'lutschen',
+        translating: 'ssać'
+    }, {
+        id: 49,
+        word: 'schlucken',
+        translating: 'połykać'
+    }, {
+        id: 50,
+        word: 'rezeptfrei',
+        translating: 'bez recepty'
+    }, {
+        id: 51,
+        word: 'einmal',
+        translating: 'raz'
+    }, {
+        id: 52,
+        word: 'dreimal täglich',
+        translating: 'trzy raz dziennie'
+    }, {
+        id: 53,
+        word: 'alle vier Stunden',
+        translating: 'co cztery godziny'
+    }, {
+        id: 54,
+        word: 'vor dem Essen',
+        translating: 'przed jedzeniem'
+    }, {
+        id: 55,
+        word: 'nach dem Essen',
+        translating: 'po jedzeniu'
+    }, {
+        id: 56,
+        word: 'Tabletten gegen Grippe',
+        translating: 'tabletki na grypę'
+    }, {
+        id: 57,
+        word: 'Arm',
+        translating: 'ręka'
+    }, {
+        id: 58,
+        word: 'Blut',
+        translating: 'krew'
+    }, {
+        id: 59,
+        word: 'Finger',
+        translating: 'palec'
+    }, {
+        id: 60,
+        word: 'Messer',
+        translating: 'nóż'
+    }, {
+        id: 61,
+        word: 'Unglück',
+        translating: 'nieszczęście'
+    }, {
+        id: 62,
+        word: 'Pech',
+        translating: 'pech'
+    }, {
+        id: 63,
+        word: 'auf passen',
+        translating: 'uważać'
+    }, {
+        id: 64,
+        word: 'aus rutschen',
+        translating: 'poślizgnąć się'
+    }, {
+        id: 65,
+        word: 'sich beeilen',
+        translating: 'śpieszyć się'
+    }, {
+        id: 66,
+        word: 'bluten',
+        translating: 'krwawić'
+    }, {
+        id: 67,
+        word: 'duschen',
+        translating: 'brać prysznic'
+    }, {
+        id: 68,
+        word: 'sich konzentrieren',
+        translating: 'koncentrować się'
+    }, {
+        id: 69,
+        word: 'passieren',
+        translating: 'wydarzyć się'
+    }, {
+        id: 70,
+        word: 'fallen',
+        translating: 'spadać'
+    }, {
+        id: 71,
+        word: 'sich schneiden',
+        translating: 'zaciąć się'
+    }, {
+        id: 72,
+        word: 'schütten',
+        translating: 'nieszczęście'
+    }, {
+        id: 73,
+        word: 'sich verbrennen',
+        translating: 'poparzyć się'
+    }, {
+        id: 74,
+        word: 'gefährlich',
+        translating: 'niebezpieczny'
+    }, {
+        id: 75,
+        word: 'scharf',
+        translating: 'ostry'
+    }, {
+        id: 76,
+        word: 'stark',
+        translating: 'mocny'
+    }, {
+        id: 77,
+        word: 'tief',
+        translating: 'głeboki'
+    }, {
+        id: 78,
+        word: 'vorsichtig',
+        translating: 'ostrożny'
+    }, {
+        id: 79,
+        word: 'vom Stuhl fallen',
+        translating: 'spaść z krzesła'
+    }, {
+        id: 80,
+        word: 'sich auf einen Stuhl stellen',
+        translating: 'stanąć na krzesło'
+    }, {
+        id: 81,
+        word: 'sich tief schneiden',
+        translating: 'zaciąć się głęboko'
+    }, {
+        id: 82,
+        word: 'Vorsicht',
+        translating: 'Uwaga!'
+    }, {
+        id: 83,
+        word: 'Sei vorsichtig',
+        translating: 'Bądź ostrożny'
     }
 ]
