f9b3de93c18e69d10b08f0bba3d6807d75fb197c
Mariusz Jacek <marios.yaceg@gmail.com>
2022-12-06 20:10:37 +0100

    Update style

---
+++ src/data/health.js
@@ -0,0 +1,173 @@
+export default [{
+    id: 0,
+    word: 'ankle',
+    translating: 'kostka',
+}, {
+    id: 1,
+    word: 'arm',
+    translating: 'ramię',
+}, {
+    id: 2,
+    word: 'bitten',
+    translating: 'pogryziony',
+}, {
+    id: 3,
+    word: 'blocked nose',
+    translating: 'zatkany nos',
+}, {
+    id: 4,
+    word: 'bottom',
+    translating: 'pupa, siedzenie',
+}, {
+    id: 5,
+    word: 'break',
+    translating: 'złamanie',
+}, {
+    id: 6,
+    word: 'burn',
+    translating: 'oparzenie',
+}, {
+    id: 7,
+    word: 'burn your hair',
+    translating: 'przypalić sobie włosy',
+}, {
+    id: 8,
+    word: 'cheek',
+    translating: 'policzek',
+}, {
+    id: 9,
+    word: 'chest',
+    translating: 'klatka piersiowa',
+}, {
+    id: 10,
+    word: 'chin',
+    translating: 'broda',
+}, {
+    id: 11,
+    word: 'comb your hair',
+    translating: 'czesać włosy',
+}, {
+    id: 12,
+    word: 'cut your finger',
+    translating: 'skaleczyć się w palec',
+}, {
+    id: 13,
+    word: 'dislocate your hip',
+    translating: 'zwichnąć biodro',
+}, {
+    id: 14,
+    word: 'elbow',
+    translating: 'łokieć',
+}, {
+    id: 15,
+    word: 'eyebrow',
+    translating: 'brew',
+}, {
+    id: 16,
+    word: 'finger',
+    translating: 'palec (u ręki)',
+}, {
+    id: 17,
+    word: 'fingernail',
+    translating: 'paznokieć (u ręki)',
+}, {
+    id: 18,
+    word: 'foot',
+    translating: 'stopa',
+}, {
+    id: 19,
+    word: 'forehead',
+    translating: 'czoło',
+}, {
+    id: 20,
+    word: 'get dizzy',
+    translating: 'mieć zawroty głowy',
+}, {
+    id: 21,
+    word: 'give sb a hand',
+    translating: 'pomóc komuś',
+}, {
+    id: 22,
+    word: 'have a black eye',
+    translating: 'mieć śliwę pod okiem',
+}, {
+    id: 23,
+    word: 'have a bruise',
+    translating: 'mieć stłuczenie',
+}, {
+    id: 24,
+    word: 'have a cut',
+    translating: 'mieć skaleczenie',
+}, {
+    id: 25,
+    word: 'have a snore finger',
+    translating: 'mieć bolący palec',
+}, {
+    id: 26,
+    word: 'I could`t believe my eyes',
+    translating: 'Nie mogłem uwierzyć własnym oczom',
+}, {
+    id: 27,
+    word: 'knee',
+    translating: 'kolano',
+}, {
+    id: 28,
+    word: 'laugh your head off',
+    translating: 'zrywać boki ze śmiechu',
+}, {
+    id: 29,
+    word: 'lips',
+    translating: 'usta',
+}, {
+    id: 30,
+    word: 'neck',
+    translating: 'szyja',
+}, {
+    id: 31,
+    word: 'on the tip of your tongue',
+    translating: 'na końcu języka',
+}, {
+    id: 32,
+    word: 'poke',
+    translating: 'szturchnąć',
+}, {
+    id: 33,
+    word: 'pull somebody`s leg',
+    translating: 'nabierać kogoś',
+}, {
+    id: 34,
+    word: 'rib',
+    translating: 'żebro',
+}, {
+    id: 35,
+    word: 'shoulder',
+    translating: 'bark',
+}, {
+    id: 36,
+    word: 'spine',
+    translating: 'kręgosłup',
+}, {
+    id: 37,
+    word: 'sprain your ankle',
+    translating: 'skręcić kostkę',
+}, {
+    id: 38,
+    word: 'thigh',
+    translating: 'udo',
+}, {
+    id: 39,
+    word: 'thumb',
+    translating: 'kciuk',
+}, {
+    id: 40,
+    word: 'toe',
+    translating: 'palec (u nogi)',
+}, {
+    id: 41,
+    word: 'waist',
+    translating: 'talia',
+}, {
+    id: 42,
+    word: 'wrist',
+    translating: 'nadgarstek',
+}]
